b5d3308ba73aeb782a409f6e07a8bf9e309306dc
nhchung11 <ngchung2002bn@gmail.com>
2023-11-02 11:47:46 +0700

    forth commit

--- test2.py
+++ test2.py
@@ -1,3 +1,15 @@
-print("test")
-print("Nguyen Hoang Chung")
-print("abc")
+import my_lib
+import numpy as np
+import cv2
+
+# Lấy ma trận từ file bin và tham số
+bin_path = r'D:\python\wavelet_intern\data\bin4.bin'
+param_path = r'D:\python\wavelet_intern\data\213622251778.txt'
+
+name, depth_scale, depth_width, depth_height, depth_cx, depth_cy, depth_fx, depth_fy = my_lib.get_parameter(param_path)
+depth_data = my_lib.convert(bin_path, depth_width, depth_height)
+
+
+my_lib.o3d_visualize(depth_data, depth_width, depth_height, depth_scale, depth_cx, depth_cy, depth_fx, depth_fy)
+
+
