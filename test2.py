a8a714474bd7b5aba72118374b0a54611fecc6ba
nhchung11 <ngchung2002bn@gmail.com>
2023-11-03 11:53:39 +0700

    add new bin and param

--- test2.py
+++ test2.py
@@ -3,8 +3,8 @@ import numpy as np
 import cv2
 
 # Lấy ma trận từ file bin và tham số
-bin_path = r'D:\python\wavelet_intern\data\bin6.bin'
-param_path = r'D:\python\wavelet_intern\data\param2.txt'
+bin_path = r'D:\python\wavelet_intern\data\bin7.bin'
+param_path = r'D:\python\wavelet_intern\data\param3.txt'
 
 name, depth_scale, depth_width, depth_height, depth_cx, depth_cy, depth_fx, depth_fy = my_lib.get_parameter(param_path)
 depth_data = my_lib.convert(bin_path, depth_width, depth_height)
