8406ed325917bbf68adfeae54edf05985d5b65be
nhchung11 <99390111+nhchung11@users.noreply.github.com>
2023-12-01 15:14:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ depth_data = my_lib.convert(bin_path, depth_width, depth_height)
 original = depth_data.copy()
 points = []
 # Lấy giá trị 4 góc và giá trị trung bình của 2 thanh
-leftbar, leftbar_avr, upper_left, bottom_left = my_lib.get_first_bar(depth_data, depth_height, depth_width)
-rightbar_avr, upper_right, bottom_right = my_lib.get_second_bar(depth_data, depth_height, depth_width, leftbar_avr, leftbar)
+leftbar, leftbar_avr, upper_left, bottom_left = my_lib.get_first_bar(depth_data)
+rightbar_avr, upper_right, bottom_right = my_lib.get_second_bar(depth_data, leftbar_avr, leftbar)
 
 # Điều chỉnh 4 góc 
-upper_left, bottom_left, upper_right, bottom_right = my_lib.get_4_corner_of_body(upper_left, bottom_left, upper_right, bottom_right, depth_height, depth_width)
+upper_left, bottom_left, upper_right, bottom_right = my_lib.get_4_corner_of_body(original, upper_left, bottom_left, upper_right, bottom_right)
 
 if upper_left[1] < depth_height / 2:
     original[:upper_left[1], :] = 0
@@ -45,7 +45,7 @@ for i in range(depth_width):
 
 
 # Cắt ảnh theo 4 góc
-original = my_lib.cut(original, upper_left, bottom_left, upper_right, bottom_right, depth_width, depth_height)
+original = my_lib.cut(original, upper_left, bottom_left, upper_right, bottom_right)
 
 body = original.copy()
 line2d, belly1, belly2, tail2d = my_lib.get_backbone_line(body)
@@ -77,15 +77,16 @@ tail3d[1] = tmp
 point1, point2 = my_lib.get_point_and_nearby(tail3d, line3d, depth_height)
 
 
-p1, p2, p3 = my_lib.get3points(original, point1, point2, depth_width, depth_height)
+p1, p2, p3 = my_lib.get3points(original, point1, point2)
 d1, d2, arcos_degree = my_lib.get_result(p1, p2, p3)
-points.append(tail3d)
+# points.append(tail3d)
 points.append(p1)
 points.append(p2)
 points.append(p3)
-
+point = my_lib.get_tail3d(original, line3d)
+points.append(point)
 print(f"Rho1: {d1}")
 print(f"Rho2: {d2}")
 print(f"Góc lệch: {arcos_degree}")
 
-my_lib.plt_visualize(original, depth_width, depth_height, points) 
+# my_lib.plt_visualize(original, points) 
